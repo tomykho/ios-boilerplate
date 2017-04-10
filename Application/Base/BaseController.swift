0b2f8a4535ba40ff4d9f05028b28ba498a367711
Tomy Kho <tomy.kho.xwl@gmail.com>
2017-04-10 16:47:51 +0700

    Changed BaseController root view to addSubnode instead

--- Application/Base/BaseController.swift
+++ Application/Base/BaseController.swift
@@ -17,13 +17,18 @@ class BaseController<L>: UIViewController where L: ASDisplayNode {
     }
     
     override func loadView() {
+        super.loadView()
         layoutNode = self.createView()
-        self.view = layoutNode.view
+        self.view.addSubnode(layoutNode)
         if let delegate = UIApplication.shared.delegate {
             self.view.backgroundColor = delegate.window??.backgroundColor
         } else {
             self.view.backgroundColor = .white
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.layoutNode.frame = self.view.bounds
     }
     
     override func didMove(toParentViewController parent: UIViewController?) {
